6458b3b3962485c4308552ae0aae1415e76f8d6a
hennanedouaa <ld_hennane@esi.dz>
2024-01-02 20:19:41 +0100

    revolve conflict with main

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,3 @@
-# app/__init__.py
 from flask import Flask
 from flask_migrate import Migrate
 from flask_restx import Api
@@ -9,8 +8,8 @@ from flask_mail import Mail
 from config import Config
 from .database import db
 from .models.user import User
-from .routes.users_manager import users_bp
 from .routes import init_routes,init_jwt,init_route_admin
+from .routes.users_manager import users_bp
 from .routes.article_manager import article_manager
 
 from .engine.es import ESKNN
