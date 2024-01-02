355ec9b63bcaeed9f92c49b6e0ae79ad796489c5
amel-FETTAH <amelfettah43@gmail.com>
2024-01-02 12:44:52 +0100

    commit

--- client/src/component/PopoutArticle.jsx
+++ client/src/component/PopoutArticle.jsx
@@ -28,32 +28,60 @@ import React, { useState } from 'react';
     const handleCloseClick = () => {
       setIsContainerOpen(false);
     };
+    
+    const handleUpload = () => {
+      // Handle the search button click
+      // ...
+    };
   
     return (
       <>
         {isContainerOpen && (
-        
-        <div className="bg-[#BEB9A1] w-full md:w-[80%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%] md:min-h-[70vh] h-screen   overflow-hidden flex flex-col justify-center mx-auto mt-10 md:mt-[10%] mb-10 md:mb-[10%] px-4 md:px-8 py-5 rounded-md relative">
+
+      <div> 
+        <div className="bg-[#BEB9A1] w-[90%] lg:w-[40%]  md:min-h-[70vh] min-h[70dvh]    flex flex-col justify-center  mx-[5%] mt-[9rem] lg:mx-auto  lg:mt-[10%] lg:mb-[10%] px-4 lg:px-8 lg:py-7 rounded-[20px] relative">
           <img src={closeIcon} alt="Close Icon" className="absolute top-0 right-0 cursor-pointer " style={{ width: '16px', height: '15px', marginRight: '14px', marginTop: '15px' }} onClick={handleCloseClick} />
-          <div className="grid-cols-[1fr,1fr] gap-[20px] lg:grid-cols-[5fr,1fr,1fr] grid">
+          <div className="grid-cols-1 gap-[20px] lg:grid-cols-[5fr,1fr,1fr] grid">
             <div className="flex items-center">
-              <h1 className="text-black font-merriweather text-2xl text-left mt-5 ml-5 font-semibold">
+              <h1 className="text-black font-merriweather text-xl lg:text-2xl text-left mt-5 ml-5 font-semibold">
                 Title: {articleData.title}
               </h1>
             </div>
-            <div className="flex items-center space-x-4 mr-4">
-              <button className="flex justify-center items-center bg-[#395143] flex-shrink-0 p-3 rounded-md">
-                <span className="text-[#E7E4D5] text-md font-semibold">Ajouter aux Favoris</span>
+            <div className="flex items-center space-x-4 mr-[2rem]">
+            <div className=' justify-center items-center bg-[#395143] flex-shrink-0 p-3 rounded-md transform transition-transform duration-200 ease-in-out hover:scale-110 lg:flex hidden' >
+               <button
+                onClick={handleUpload}
+                className="text-[#E7E4D5] text-md font-semibold">Ajouter aux Favoris
               </button>
-              <button className="flex justify-center items-center bg-[#395143] flex-shrink-0 p-3 rounded-md ">
-                <span className="text-[#E7E4D5] text-md font-semibold ">Voir PDF</span>
+            </div>  
+             <div className='justify-center items-center bg-[#395143] flex-shrink-0 p-3 rounded-md transform transition-transform duration-200 ease-in-out hover:scale-110 lg:flex hidden' >
+              <button
+               onClick={handleUpload}
+               className="text-[#E7E4D5] text-md font-semibold ">Voir PDF
               </button>
+              </div>
             </div>
           </div>
           <div className="overflow-y-auto p-4 h-[500px] text-black font-lora text-xl font-normal text-left ml-0 ">
             {articleData.description}
           </div>
         </div>
+      <div className="flex items-center space-x-4 ml-[8rem] mt-[1rem]  ">
+        <div className=' justify-center items-center bg-[#395143] flex-shrink-0 p-3 rounded-md transform transition-transform duration-200 ease-in-out hover:scale-110 lg:hidden flex' >
+           <button
+            onClick={handleUpload}
+            className="text-[#E7E4D5] text-md font-semibold">Ajouter aux Favoris
+          </button>
+        </div>  
+         <div className='justify-center items-center bg-[#395143] flex-shrink-0 p-3 rounded-md transform transition-transform duration-200 ease-in-out hover:scale-110 lg:hidden flex ' >
+          <button
+           onClick={handleUpload}
+           className="text-[#E7E4D5] text-md font-semibold ">Voir PDF
+          </button>
+          </div>
+        </div> 
+       </div>
+
       )}
     </>
   );
